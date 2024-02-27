d164f0309c511ee4b220faf686cf09597cb826b7
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-02-27 08:43:42 -0600

    Distancia entre dos Puntos (con variables)

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main(void){
-    float x1,y1,x2,y2,m;
+    float x1,y1,x2,y2,d, s, p1, p2;
 
     cout << "Ingresa x1 ";
     cin >> x1;
@@ -13,11 +14,15 @@ int main(void){
     cout << "Ingresa y2 ";
     cin >> y2;
 
-    m = (y2-y1)/(x2-x1);
+    p1 = pow(y2-y1,2);
+    p2 = pow(x2-x1,2);
+    s = p1+p2;
+
+    d = sqrt(  s  );
 
     cout << "P1(" << x1 << "," << y1 << ")" << endl;
     cout << "P2(" << x2 << "," << y2 << ")" << endl;
-    cout << "m = "<< m << endl;
+    cout << "d = "<< d << endl;
 
     return 0;
 }
